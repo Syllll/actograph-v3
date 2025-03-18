077c3a3b6741f359598616f1be824c04636d24eb
Sylvain Meylan <sylvain.meylan@improba.fr>
2025-03-18 09:30:31 +0100

    fix(scripts): fix version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -16,10 +16,10 @@ if [ -z "$1" ]; then
     echo "No argument provided, for example prod"
     exit 1
 fi
-versionType="$1"
+deployType="$1"
 
 # Check if the first argument is prod, we only support prod for now
-if [ "$versionType" != "prod" ]; then
+if [ "$deployType" != "prod" ]; then
     echo "Invalid argument, must be prod"
     exit 1
 fi
@@ -30,25 +30,25 @@ fi
 # - patch
 # If no argument is provided, it is patch
 if [ -z "$2" ]; then
-    versionType="patch"
+    deployType="patch"
 else
-    versionType="$2"
+    deployType="$2"
 
-    if [ "$versionType" != "major" ] && [ "$versionType" != "minor" ] && [ "$versionType" != "patch" ]; then
+    if [ "$deploynType" != "major" ] && [ "$deployType" != "minor" ] && [ "$deployType" != "patch" ]; then
         echo "Invalid version type, must be major, minor or patch"
         exit 1
     fi
 fi
 
-# Print the version type
-echo "Version type: $versionType"
+# Print the deploy type
+echo "Deploy type: $deployType"
 
 # Get script directory in a cross-platform way
 scriptFolderPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Get the version from the package.json of front and api
-frontVersion=$(cat $scriptFolderPath/front/package.json | jq -r '.version')
-apiVersion=$(cat $scriptFolderPath/api/package.json | jq -r '.version')
+frontVersion=$(cat $scriptFolderPath/../front/package.json | jq -r '.version')
+apiVersion=$(cat $scriptFolderPath/../api/package.json | jq -r '.version')
 
 # Print the versions
 echo "Front version: $frontVersion"
@@ -75,21 +75,26 @@ fi
 # Print the new version
 echo "New version: $frontVersion"
 
-# Update the version in the package.json of front and api
-sed -i '' "s/\"version\": \"$frontVersion\"/\"version\": \"$frontVersion\"/g" $scriptFolderPath/front/package.json
-sed -i '' "s/\"version\": \"$frontVersion\"/\"version\": \"$frontVersion\"/g" $scriptFolderPath/api/package.json
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_CMD="sed -i ''"
+else
+    SED_CMD="sed -i"
+fi
+
+$SED_CMD "s/\"version\": \"$frontVersion\"/\"version\": \"$frontVersion\"/g" "$scriptFolderPath/../front/package.json"
+$SED_CMD "s/\"version\": \"$frontVersion\"/\"version\": \"$frontVersion\"/g" "$scriptFolderPath/../api/package.json"
 
 # Make a version commit and push it to the remote repository
-git add $scriptFolderPath/front/package.json
-git add $scriptFolderPath/api/package.json
+git add $scriptFolderPath/../front/package.json
+git add $scriptFolderPath/../api/package.json
 git commit -m "Bump version to $frontVersion"
 git push
 
 # Create a new tag with format prod-vX.Y.Z
-git tag prod-v$frontVersion
+git tag ${deployType}-v$frontVersion
 
 # Push the tag to the remote repository
-git push origin ${versionType}-v$frontVersion
+git push origin ${deployType}-v$frontVersion
 
 # All done
 echo "All done, the CI will now run on GitHub"
