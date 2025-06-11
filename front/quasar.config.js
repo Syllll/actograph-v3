3532f0c60537ba2a9d6a12cd8550677263ee67a7
Sylvain Meylan <sylvain.meylan@improba.fr>
2025-06-11 16:10:28 +0200

    feat(front): clean

--- front/quasar.config.js
+++ front/quasar.config.js
@@ -340,7 +340,7 @@ module.exports = configure(function (/* ctx */) {
           category: 'public.app-category.utilities',
         },
         win: {
-          target: 'msi', // nsis
+          target: 'nsi',
           // Valid compression values are: "maximum", "normal", "store"
           // store: fastest but biggest size
           // normal: default
