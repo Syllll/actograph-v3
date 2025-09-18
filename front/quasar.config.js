04f1999daf2ea8ce8188714406b6a4b90e09eb5c
Sylvain Meylan <sylvain@ateliom.fr>
2025-09-18 14:42:00 +0200

    fix

--- front/quasar.config.js
+++ front/quasar.config.js
@@ -338,6 +338,7 @@ module.exports = configure(function (/* ctx */) {
         copyright: '©2025 SymAlgo Technologies',
         mac: {
           category: 'public.app-category.utilities',
+          universal: true,
         },
         win: {
           target: 'nsis',
